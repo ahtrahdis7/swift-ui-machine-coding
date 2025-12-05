33c1c0a09f41b0287053820a4c94f2deed872723
ahtrahdis7 <mallicksidhartha7@gmail.com>
2025-12-05 23:59:29 +0530

    Initial Commit

---
+++ Q1-TodoAppTry1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Q1-TodoAppTry1
+//
+//  Created by Sidhartha Mallick on 05/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Q1-TodoAppTry1/Q1_TodoAppTry1App.swift
@@ -0,0 +1,17 @@
+//
+//  Q1_TodoAppTry1App.swift
+//  Q1-TodoAppTry1
+//
+//  Created by Sidhartha Mallick on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Q1_TodoAppTry1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
